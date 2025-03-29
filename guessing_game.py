8675504d0bb34e032d92f5b1dca7c110e501603b
kiu_gorozia.nika <gorozia.nika@kiu.edu.ge>
2025-03-29 14:46:28 +0400

    commit

---
+++ guessing_game.py
@@ -0,0 +1,23 @@
+# guessing_game.py
+import random
+
+def main():
+    number = random.randint(1, 100)
+    guesses = 0
+
+    print("I'm thinking of a number between 1 and 100. Can you guess it?")
+
+    while True:
+        guess = int(input("Your guess: "))
+        guesses += 1
+
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        else:
+            print(f"Congratulations! You guessed it in {guesses} tries!")
+            break
+
+if __name__ == "__main__":
+    main()
