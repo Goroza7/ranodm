ed4df221defea9283f112261c77d84aa09fe7cd5
kiu_gorozia.nika <gorozia.nika@kiu.edu.ge>
2025-03-29 14:35:56 +0400

    commit.

--- views.py
+++ views.py
@@ -8,7 +8,7 @@ print("I'm thinking of a number between 1 and 100. Can you guess it?")
 while True:
     guess = int(input("Your guess: "))
     guesses += 1
-    
+
     if guess < number:
         print("Too low!")
     elif guess > number:
