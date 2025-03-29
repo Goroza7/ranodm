9ae398cf42fe78f06dcd2d2f40dfddf15d3f8682
Goroza7 <77842614+Goroza7@users.noreply.github.com>
2025-03-29 14:24:04 +0400

    Update views.py

--- views.py
+++ views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+number = random.randint(1, 100)
+guesses = 0
+
+print("I'm thinking of a number between 1 and 100. Can you guess it?")
+
+while True:
+    guess = int(input("Your guess: "))
+    guesses += 1
+    
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print(f"Congratulations! You guessed it in {guesses} tries!")
+        break
