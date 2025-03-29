012e9817d2388b51f723466cc92571c584e45d23
kiu_gorozia.nika <gorozia.nika@kiu.edu.ge>
2025-03-29 15:22:52 +0400

    uk

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,11 @@
 # tests/test_game.py
 import random
-import os
 import sys
+import os
+
+# Add the directory containing guessing_game.py to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from guessing_game import main
 
 # Add the repository root directory to sys.path
