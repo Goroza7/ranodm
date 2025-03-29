1abdbee5715f9e17ef459f48bbc0985293722e4d
kiu_gorozia.nika <gorozia.nika@kiu.edu.ge>
2025-03-29 16:40:41 +0400

    commit

--- hello_world.py
+++ hello_world.py
@@ -4,17 +4,16 @@ from pyramid.response import Response
 import os
 
 def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "Hello, " + name + "!\n"
-    return Response(message)
+    name = os.environ.get('NAME', 'world')
+    return Response(f"Hello, {name}!\n")
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
+    
     server = make_server('0.0.0.0', port, app)
+    print(f"Serving on port {port}...")
     server.serve_forever()
